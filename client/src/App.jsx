b579152df6217f28da61e408befe632eaf3668ac
Liam Lennon-Flynn <82933053+imliamflynn@users.noreply.github.com>
2025-08-10 12:28:10 +1200

    Added tailwindcss, postcss, and autoprefixer as dev dependencies.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
-import './App.css'; // Import basic styling (Vite handles CSS imports)
 import SubscriptionsDashboard from './components/SubscriptionsDashboard';
+//import './App.css'; // Import basic styling (Vite handles CSS imports)
+//import './index.css'; // Import Tailwind CSS styles
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Finance Tracker</h1>
+        <h1 className="text-red-500">Finance Tracker</h1>
       </header>
       <SubscriptionsDashboard />
       <footer>
