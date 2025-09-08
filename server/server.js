ec61a0999ac70145ca8498e4f7e62462f572b00d
imliamflynn <imliamflynn@gmail.com>
2025-09-08 11:25:59 +0000

    Added .gitignore and DB credentials

--- server/server.js
+++ server/server.js
@@ -5,7 +5,15 @@ const cors = require("cors");
 const multer = require("multer");
 const fs = require("fs");
 const csv = require("csv-parser");
-const pool = require("./utils/db"); // import the db connection
+//const pool = require("./utils/db"); // import the db connection
+require('dotenv').config();
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
 const path = require("path");
 const detectSubscriptions = require("./utils/detectSubscriptions"); // import the subscription detection logic
 const detectBank = require("./utils/detectBank"); // import the bank detection utility
